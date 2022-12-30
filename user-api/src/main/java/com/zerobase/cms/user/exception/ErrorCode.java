63344b629b9c0e82c8d8e2796f41a5126242fa59
dksekfbs72 <dksekfbs72@naver.com>
2022-12-30 21:20:16 +0900

    feat : [004-LOGIN-CUSTOMER] 커스터머 로그인 구현.
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
+++ user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
@@ -11,7 +11,12 @@ public enum ErrorCode {
 	ALREADY_VERIFY(HttpStatus.BAD_REQUEST, "이미 인증이 완료되었습니다."),
 	EXPIRE_CODE(HttpStatus.BAD_REQUEST, "인증 시간이 만료되었습니다."),
 	WRONG_VERIFICATION(HttpStatus.BAD_REQUEST, "잘못된 인증 시도입니다."),
-	USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저 정보를 찾을 수 없습니다."),;
+	USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저 정보를 찾을 수 없습니다."),
+
+	//login
+	LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "패스워드를 확인해주세요."),
+
+	;
 
 	private final HttpStatus httpStatus;
 	private final String detail;

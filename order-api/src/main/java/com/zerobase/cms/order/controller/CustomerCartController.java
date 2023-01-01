2c910bf876c18f58f0b3aae78ebfe849215c0ac8
dksekfbs72 <dksekfbs72@naver.com>
2023-01-02 01:33:31 +0900

    feat : [016-PAYMENT] 카트 결제

--- order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
+++ order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
@@ -1,10 +1,12 @@
 package com.zerobase.cms.order.controller;
 
 import com.zerobase.cms.order.application.CartApplication;
+import com.zerobase.cms.order.application.OrderApplication;
 import com.zerobase.cms.order.domain.product.AddProductCartForm;
 import com.zerobase.cms.order.domain.redis.Cart;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +23,7 @@ public class CustomerCartController {
 
 	private final CartApplication cartApplication;
 	private final JwtAuthenticationProvider provider;
+	private final OrderApplication orderApplication;
 
 	@PostMapping
 	public ResponseEntity<Cart> addCart(
@@ -40,5 +43,14 @@ public class CustomerCartController {
 		@RequestHeader(name = "X-AUTH-TOKEN") String token,
 		@RequestBody Cart cart){
 		return ResponseEntity.ok(cartApplication.updateCart(provider.getUserVo(token).getId(),cart));
+	}
+
+	@PostMapping("/order")
+	public ResponseEntity<Cart> Payment(
+		@RequestHeader(name = "X-AUTH-TOKEN") String token,
+		@RequestBody Cart cart
+	) {
+		orderApplication.order(token,cart);
+		return ResponseEntity.ok().build();
 	}
 }
--- order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductItemRepository.java
+++ order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductItemRepository.java
@@ -1,10 +1,11 @@
 package com.zerobase.cms.order.domain.repository;
 
 import com.zerobase.cms.order.domain.model.ProductItem;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
-
+	ProductItem getById(Long id);
 }
